969d4b7491f49ca5f9012002d77b82b509c75da9
es_python <espython85@gmail.com>
2023-05-27 20:59:12 +0300

    add logo

---
+++ app/components/container/container.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export interface IContainerProps {
+  children: React.ReactNode;
+}
+
+function container({ children }: IContainerProps) {
+  return (
+    <div
+      className="
+    max-w-[2520px] 
+    max-auto 
+    xl:px-20 
+    md:px-10 
+    sm:px-2 px-4"
+    >
+      {children}
+    </div>
+  );
+}
+
+export default container;
